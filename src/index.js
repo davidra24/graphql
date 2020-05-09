ce52752d9571a060783d30db31858cafc7c72a4a
David Ramírez <cdavidramirezg24@gmail.com>
2020-05-09 00:58:10 -0500

    Use of files with .graphql ext

--- src/index.js
+++ src/index.js
@@ -1,21 +1,29 @@
-import { graphql, buildSchema } from 'graphql';
+import { buildSchema, __TypeKind } from 'graphql';
+import express from 'express';
+import gqlMiddleware from 'express-graphql';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import { resolvers } from '../lib/resolvers';
+
+const app = express();
+const port = process.env.port || 3000;
 
 //Esquema - Define que va a hacer el API en GrapQL
 //Query - consutas de información y el retorno
-const schema = buildSchema(`
-    type Query {
-        hello: String,
-        saludo: String
-    }
-`);
-
-//Configura los resolvers
-const resolvers = {
-  hello: () => 'Hola mundo',
-  saludo: () => 'Hola a todos',
-};
-
-//Ejecuta el query (esquema, nombreQuery)
-graphql(schema, '{ hello, saludo }', resolvers).then((data) =>
-  console.log(data)
+const schema = buildSchema(
+  readFileSync(join(__dirname, '../lib', 'schema.graphql'), 'utf-8')
 );
+
+//Ejecutamos el middlewate
+app.use(
+  '/api',
+  gqlMiddleware({
+    schema,
+    rootValue: resolvers,
+    graphiql: true,
+  })
+);
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/api`);
+});
